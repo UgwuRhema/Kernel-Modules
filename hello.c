5ae8a6974971a596858362791a51a24cf216db83
UgwuRhema <rhemauros@protonmail.com>
2026-09-07 17:27:56 +0000

    Finished the hello.c code for the module, time to build

--- hello.c
+++ hello.c
@@ -6,3 +6,17 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Rhema");
 MODULE_DESCRIPTION("Just a minimal kernel module to get going");
 MODULE_VERSION("1.0");
+
+static int __init hello_init(void)
+{
+    printk(KERN_INFO "Hello World from kernel Space!\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk(KERN_INFO "Goodbye World from kernel Space!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
