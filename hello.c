ce6c4446376f7951c50ef5540aeb3fd5b9e296b7
UgwuRhema <rhemauros@protonmail.com>
2026-09-07 17:19:15 +0000

    Initial start, making minimal Hello World module

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rhema");
+MODULE_DESCRIPTION("Just a minimal kernel module to get going");
+MODULE_VERSION("1.0");
