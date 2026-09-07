ff9b1474a519aa337020329153d3c381c7ec0dc6
UgwuRhema <rhemauros@protonmail.com>
2026-09-07 17:40:14 +0000

    Made the hello whatever module .ko file

---
+++ .Module.symvers.cmd
@@ -0,0 +1,1 @@
+savedcmd_Module.symvers :=  /usr/src/linux-6.18.43-gentoo-dist-bin/scripts/mod/modpost -M          -o Module.symvers -n -T modules.order -i /usr/src/linux-6.18.43-gentoo-dist-bin/Module.symvers -e 
