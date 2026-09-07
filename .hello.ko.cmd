b08ea0df6de1a9009944de967d588718e82d94eb
UgwuRhema <rhemauros@protonmail.com>
2026-09-07 17:44:54 +0000

    modified .gitignore

--- .hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_hello.ko := ld -r -m elf_x86_64 --compress-debug-sections=zlib -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/linux-6.18.43-gentoo-dist-bin/scripts/module.lds -o hello.ko hello.o hello.mod.o .module-common.o
