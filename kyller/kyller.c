0f475fa765d9ba586708486dcaa35b0db26fef13
UgwuRhema <rhemauros@protonmail.com>
2026-09-09 20:46:07 +0000

    Kyller1 module made, harmful module that triggers a page fault because of writing to a null pointer

---
+++ kyller/kyller.c
@@ -0,0 +1,29 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rhma");
+MODULE_DESCRIPTION("Kyller1 module for testing exploits on kernel");
+MODULE_VERSION("0.1");
+
+static int __init buggy_init(void)
+{
+    int *ptr = NULL;
+
+    pr_info("buggy_module: Loading module and dereferencing NULL...\n");
+
+	//as you can see, this is a null pointer write
+	//this should...trigger a page fault, let's see
+    *ptr = 0xDEADBEEF;
+
+    return 0;
+}
+
+static void __exit buggy_exit(void)
+{
+    pr_info("buggy_module: Unloading module\n");
+}
+
+module_init(buggy_init);
+module_exit(buggy_exit);
