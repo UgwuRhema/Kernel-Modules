ed814a52533a161c849f367612f41a10219fb542
UgwuRhema <rhemauros@protonmail.com>
2026-09-09 10:14:55 +0000

    depmod is really misbehaving

--- firstmod/firstmod.c
+++ firstmod/firstmod.c
@@ -2,20 +2,20 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-MODULE_AUTHOR("Some Random Guy");
+MODULE_AUTHOR("Someone");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("The first custom loadable module");
 MODULE_VERSION("1.0.0");
+MODULE_DESCRIPTION("Just a module");
 
 static int __init start(void)
 {
-	printk(KERN_INFO "The First Custom Module here!\n");
+	printk(KERN_INFO "Custom module loaded here!\n");
 	return 0;
 }
 
 static void __exit stop(void)
 {
-	printk(KERN_INFO "The First Custom Module has now been removed!\n");
+	printk(KERN_INFO "Custom module unloaded here!\n");
 }
 
 module_init(start);
