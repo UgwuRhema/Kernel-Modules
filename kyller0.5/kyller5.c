f02fda80d96c89d3acd8ecc706d9d42bd741cb0b
UgwuRhema <rhemauros@protonmail.com>
2026-09-10 11:03:33 +0000

    Kyller 5 seems to give me some compilation issues

---
+++ kyller0.5/kyller5.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_AUTHOR("Rhma");
+MODULE_DESCRIPTION("Kyller0.5 exploit module, a different approach");
+MODULE_VERSION("0.5");
+MODULE_LICENSE("GPL");
+
+//we are taking a differnet approach...
+//we will do an infinite recursion so as to cause a double fault
+//i understand the concepts of CPU exception since ive made them before for my OS
+//we will trigger as many recursions as possible to trigger a page fault, then when the kernel
+//says like okay i dont got no resources anymore it raises the exception #PF the another one will run
+//and haha 2 fault = double faults...definiately panic! kyller0.5 is not failing
+
+static void __attribute__((noinline)) recurse(void)
+{
+	recurse(); //recursion infintely...
+}
+
+static int __init boom(void)
+{
+	printk(KERN_INFO "Kyller0.5 in action, ultimate recursion!\n");
+	recurse();
+	return 0;
+}
+
+//who knows if this would be possible..but i doubt
+//the recursions might happen so quickly, and system might even get buggy just liek a fork bomb
+static void __exit exit(void)
+{
+	printk(KERN_INFO "Will this even be reachable? lets find out, I AM EXIT!\n");
+}
+
+module_init(boom);
+module_exit(exit);
